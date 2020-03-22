a4666876272edbf2e7f4c744773459418ea05666
Christian Maines <christian1006@gmail.com>
2020-03-22 22:42:46 +0100

    the right code

---
+++ Desktop/brackets-master/src/index.js
@@ -0,0 +1,22 @@
+module.exports = function check(str, bracketsConfig) {
+  let i, l=str.length,  char,  last, stack=[];
+
+  for(i=0; i<l; i++){
+      char=str[i];
+      if(char=="{" || char=="("){
+          stack.push(char);
+          last=char;
+      }else if(char == '}' || char == ")"){
+          if(last){
+              if((char == '}' && last == "{") || (char == ')' && last == '('))
+              {
+                  stack.pop();
+                  last = stack.length > 0 ? stack[stack.length - 1] : undefined;
+              }
+          }else{
+              return false;
+          }
+      }
+  }
+  return stack.length==0;
+}
